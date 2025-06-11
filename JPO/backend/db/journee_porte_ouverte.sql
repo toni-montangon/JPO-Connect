3548817dc12730d3407a919bf8f50180c1a2f9de
HEUREUX Axel <axel.heureux@laplateforme.io>
2025-06-11 13:07:31 +0200

    📝 Ajout de la BDD

---
+++ JPO/backend/db/journee_porte_ouverte.sql
@@ -0,0 +1,114 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1:3306
+-- Généré le : mer. 11 juin 2025 à 08:10
+-- Version du serveur : 9.1.0
+-- Version de PHP : 8.3.14
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `journee_porte_ouverte`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `commentaire`
+--
+
+DROP TABLE IF EXISTS `commentaire`;
+CREATE TABLE IF NOT EXISTS `commentaire` (
+  `id_commentaire` int NOT NULL AUTO_INCREMENT,
+  `contenu` text NOT NULL,
+  `date_commentaire` datetime NOT NULL,
+  `reponse_moderateur` text,
+  `id_jpo` int DEFAULT NULL,
+  `id_utilisateur` int DEFAULT NULL,
+  PRIMARY KEY (`id_commentaire`),
+  KEY `id_jpo` (`id_jpo`),
+  KEY `id_utilisateur` (`id_utilisateur`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `inscription`
+--
+
+DROP TABLE IF EXISTS `inscription`;
+CREATE TABLE IF NOT EXISTS `inscription` (
+  `id_inscription` int NOT NULL AUTO_INCREMENT,
+  `date_inscription` date NOT NULL,
+  `est_present` tinyint(1) DEFAULT '0',
+  `id_jpo` int DEFAULT NULL,
+  `id_utilisateur` int DEFAULT NULL,
+  PRIMARY KEY (`id_inscription`),
+  KEY `id_jpo` (`id_jpo`),
+  KEY `id_utilisateur` (`id_utilisateur`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `jpo`
+--
+
+DROP TABLE IF EXISTS `jpo`;
+CREATE TABLE IF NOT EXISTS `jpo` (
+  `id_jpo` int NOT NULL AUTO_INCREMENT,
+  `ville` varchar(100) NOT NULL,
+  `date` date NOT NULL,
+  `heure` time NOT NULL,
+  `lieu` varchar(150) NOT NULL,
+  `capacite_max` int NOT NULL,
+  PRIMARY KEY (`id_jpo`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `role`
+--
+
+DROP TABLE IF EXISTS `role`;
+CREATE TABLE IF NOT EXISTS `role` (
+  `id_role` int NOT NULL AUTO_INCREMENT,
+  `nom_role` varchar(50) NOT NULL,
+  PRIMARY KEY (`id_role`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `utilisateur`
+--
+
+DROP TABLE IF EXISTS `utilisateur`;
+CREATE TABLE IF NOT EXISTS `utilisateur` (
+  `id_utilisateur` int NOT NULL AUTO_INCREMENT,
+  `nom` varchar(100) NOT NULL,
+  `prenom` varchar(100) NOT NULL,
+  `email` varchar(150) NOT NULL,
+  `mot_de_passe` varchar(255) NOT NULL,
+  `adresse` text,
+  `id_role` int DEFAULT NULL,
+  PRIMARY KEY (`id_utilisateur`),
+  UNIQUE KEY `email` (`email`),
+  KEY `id_role` (`id_role`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
